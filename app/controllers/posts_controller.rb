790fa5d276e40b2294e926822f12f4e79beb9cae
minh <minh220500@gmail.com>
2020-09-25 16:22:35 +0700

    done skeleton

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!
+
+
+   def index
+        @posts = Post.all
+    end
+
+  
+    
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = current_user.posts.new(post_params)
+      
+      
+    if @post.save
+      flash[:success] = "Post created"
+      redirect_to action: "index"
+    else
+      render 'new'
+    end
+    end
+
+    def show
+      @post = Post.find(params[:id])
+
+    end
+
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to action: "index"
+      flash.notice = "Post Deleted!"
+    end
+
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
+end
