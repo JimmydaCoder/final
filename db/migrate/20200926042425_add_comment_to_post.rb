5f938d93555e671e7b60f6ba5cf93a24176a75b1
minh <minh220500@gmail.com>
2020-09-29 14:05:16 +0700

    second try

--- db/migrate/20200926042425_add_comment_to_post.rb
+++ db/migrate/20200926042425_add_comment_to_post.rb
@@ -1,6 +1,5 @@
 class AddCommentToPost < ActiveRecord::Migration[6.0]
   def change
-    remove_column :comments, :post_id
 
     add_reference :comments, :post, index: true, foreign_key: true
     add_reference :comments, :user, index: true, foreign_key: true
